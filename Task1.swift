645517c5884fb477b69b9562a2ed1e32a6402adc
“SmolinaNadya” <nadusha8032000@gmail.com>
2020-10-17 00:49:09 +0700

    main

--- Task1.swift
+++ Task1.swift
@@ -1,34 +1,44 @@
 
-import Foundation
-
 if CommandLine.arguments.count == 1 
 {
    
-guard let V  = readLine() else {exit(0)}
- guard let N = Double(V) else {exit(0)}
+guard let N1  = readLine() 
+else 
+{
+   exit(0)
+}
+guard let N = Double(N1) else {exit(0)}
 
- guard let M  = readLine() else
+ guard let M1  = readLine() else
  {
         exit(0)       
  }
+ guard let M = Double(N1) else {exit(0)}
 
-guard let X  = readLine() else
+guard let X1  = readLine() else
  {
         exit(0)
  }
+ guard let X = Double(X1) else {exit(0)}
 
-guard let Y  = readLine() else
+guard let Y1  = readLine() else
  {
         exit(0)
  }
- print(Task1(N:N,M:Double(M),X:Double(X),Y:Double(Y)))
+ guard let Y = Double(Y1) else {exit(0)}
+ print(Task1(N:N,M:M,X:X,Y:Y))
 }
 else 
 {
-    let N = Double(CommandLine.arguments[1])
-    let M = Double(CommandLine.arguments[2])
-    let X = Double(CommandLine.arguments[3])
-    let Y = Double(CommandLine.arguments[4])
+    let N1 = CommandLine.arguments[1]
+    let M1 = CommandLine.arguments[2]
+    let X1 = CommandLine.arguments[3]
+    let Y1 = CommandLine.arguments[4]
+    guard let N = Double(N1) else {exit(0)}
+    guard let M = Double(M1) else {exit(0)}
+    guard let X = Double(X1) else {exit(0)}
+    guard let Y = Double(Y1) else {exit(0)}
+   print(Task1(N:N,M:M,X:X,Y:Y))
 }
 
 func Task1(N:Double,M:Double,X:Double,Y:Double)->Double
