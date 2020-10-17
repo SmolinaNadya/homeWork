dd9b59fec7192f043b07f68464eb4efcd620224f
SmolinaNadya <72699421+SmolinaNadya@users.noreply.github.com>
2020-10-17 18:48:41 +0700

    Update Task5.swift

--- Task5.swift
+++ Task5.swift
@@ -3,46 +3,52 @@ var setN = Set<Int>()
 var setM = Set<Int>()
 if CommandLine.arguments.count == 1 
 {
-    guard var N = Int(readLine()!) else 
+    guard let N1 = readLine() else 
     {
         exit(0)
     }
+    guard var N = Int(N1) else { exit(0)}
     while (N>0)
     {
-         guard let i  = Int(readLine()!) else
+         guard let i1  = readLine() else
       {
         exit(0)
       }
+      guard let i = Int(i1) else { exit(0)}
     setN.insert(i)
     N-=1
     } 
-    guard var M = Int(readLine()!) else 
+    guard let M1 = readLine() else 
     {
         exit(0)
     }
+    guard var M = Int(M1) else { exit(0)}
     while (M>0)
     {
-         guard let i  = Int(readLine()!) else
+         guard let i1  = readLine() else
       {
         exit(0)
       }
+      guard let i = Int(i1) else { exit(0)}
     setM.insert(i)
     M-=1
     } 
 } 
 else 
 {
-    let N = Int(CommandLine.arguments[1])!
+    guard let N = Int(CommandLine.arguments[1]) else {exit(0)}
     var k = 0
     while (k != N)
     {
-     setN.insert(Int(CommandLine.arguments[k+2])!)
+      guard let value = Int(CommandLine.arguments[k+2]) else {exit(0)}
+     setN.insert(value)
      k+=1
     }
-    let M = Int(CommandLine.arguments[k+2])!
+    guard let M = Int(CommandLine.arguments[k+2]) else {exit(0)}
     while (k-N != M)
     {
-     setM.insert(Int(CommandLine.arguments[k+3])!)
+      guard let value = Int(CommandLine.arguments[k+3]) else {exit(0)}
+     setM.insert(value)
      k+=1
     }
 }
